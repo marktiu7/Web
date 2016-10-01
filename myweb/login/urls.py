3f07237a8e822729917167c2b620e84fd9cb0d77
marktiu <marktiu71@Outlook.com>
2016-10-01 19:40:22 +0800

    2

--- myweb/login/urls.py
+++ myweb/login/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from login import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+]
+
+urlpatterns +=[
+    url(r'^index/$',views.index,name='index'),
 ]
--- myweb/login/views.py
+++ myweb/login/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
 
 # Create your views here.
+
+def index(request):
+    mo='hello there'
+    return render_to_response('login.html',locals())
