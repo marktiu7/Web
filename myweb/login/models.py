5195ac6da6d21c1635e2eb62c4d65c41acdc8fa2
marktiu <marktiu71@Outlook.com>
2016-10-02 17:45:32 +0800

    3

--- myweb/login/models.py
+++ myweb/login/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Notice(models.Model):
+    title=models.CharField(max_length=100)
+    comment=models.TextField()
+
+    
+
