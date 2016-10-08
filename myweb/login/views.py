45c6f049d0fe565aa1e5ee338c8cad6c006e183e
marktiu71 <marktiu71@Outlook.com>
2016-10-08 15:52:50 +0800

    8

--- myweb/login/views.py
+++ myweb/login/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render,render_to_response
 from login import models
 from django.http import HttpResponseRedirect
 from pub_form.form_login import UserForm
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 
 def index(request):
+   
+    current_page=request.GET.get("page",1)
+
     mo = models.Notice.objects.all()
+    pages=Paginator(mo,6)
+    mo=pages.page(current_page)
 
     if request.method=='POST':
         uf =UserForm(request.POST)
